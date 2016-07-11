c437786efd97f3d0dc29318ca70039be6ba33377
Kaushal Elsewhere <kaushal.exe@gmail.com>
2016-07-11 16:43:00 +1000

    Generics Added

---
+++ GenericsTry/Generics.playground/Contents.swift
@@ -0,0 +1,100 @@
+//: Learn generics here
+
+import UIKit
+
+// Data Structure
+struct Queue<Element>{
+    private var elements = [Element]()
+    
+    mutating func enqueue(newElement: Element){
+        elements.append(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        guard !elements.isEmpty else{
+            return nil
+        }
+        
+        return elements.removeAtIndex(0)
+    }
+    
+}
+
+var q = Queue<Int>()
+
+q.enqueue(3)
+q.enqueue(5)
+q.enqueue(10)
+
+//Methods (with `Where` clause)
+func mid<T where T: Comparable>(array: [T]) -> T{
+    return array.sort()[(array.count - 1) / 2]
+}
+
+mid([10,20,90,50,40])
+
+//protocols
+protocol Summable {
+    func +(lhs: Self, rhs: Self) -> Self
+}
+extension Int: Summable{}
+extension Double: Summable{}
+
+func adder<T: Summable>(x: T, _ y: T) -> T{
+    return x+y
+}
+
+let intSum = adder(20, 10)
+let floatSum = adder(10.4, 9.60)
+
+extension String: Summable {}
+let stringSum = "foo"+"bar"
+
+//Extension of Generics
+extension Queue{
+    public func peek() -> Element?{
+        return elements.first
+    }
+}
+
+var qo = Queue<Int>()
+qo.enqueue(10)
+qo.enqueue(20)
+qo.enqueue(30)
+qo.enqueue(40)
+let ans = qo.peek()
+
+//Challenge: Check if all elements of the queue are equal.
+struct QueueNew<Element: Equatable>{
+    private var elements = [Element]()
+    
+    mutating func enqueue(newElement: Element){
+        elements.append(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        guard !elements.isEmpty else{
+            return nil
+        }
+        
+        return elements.removeAtIndex(0)
+    }
+}
+extension QueueNew{
+    func homogenous() -> Bool{
+        if let first = elements.first{
+            return !elements.contains{$0 != first}
+        }
+        return true
+    }
+}
+
+var quu = QueueNew<Int>()
+quu.enqueue(10)
+quu.enqueue(10)
+quu.enqueue(10)
+quu.enqueue(10)
+
+quu.homogenous()
+
+
