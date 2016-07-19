434eb5d9f97224db00b57857d41aaf757eb18ae1
Kaushal Elsewhere <kaushal.exe@gmail.com>
2016-07-19 21:27:11 +1000

    Update AppDelegate.swift

--- KeyboardObserverTry/KeyboardObserverTry/AppDelegate.swift
+++ KeyboardObserverTry/KeyboardObserverTry/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -41,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func didAcceptNotification(){
+        //
+    }
 }
 
